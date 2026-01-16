dd99ca78a2bbc1d2db01cffe76871bf962e7b348
PrajnaPradhan07 <prajna.mca98@gmail.com>
2026-01-16 12:05:04 +0530

    updated

--- habotproject/settings.py
+++ habotproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sqwq&wsc9-7&%utpc=%vzwmey^@b%)2cu3v2bmp@u-rdv*bot$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
